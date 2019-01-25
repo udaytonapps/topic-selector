6e3b91b37ddf04c44e8c8665d9cc935e9b4048aa
David P. Bauer <dbauer1@udayton.edu>
2019-01-25 15:22:49 -0500

    TSUGI-68 Initial commit for Topic Selector

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+
+// The SQL to uninstall this tool
+$DATABASE_UNINSTALL = array(
+    // Uninstall queries here
+);
+
+// The SQL to create the tables if they don't exist
+$DATABASE_INSTALL = array(
+    // Add topic selector tables here
+);
---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+
+$REGISTER_LTI2 = array(
+    "name" => "Topic Selector", // Name of the tool
+    "FontAwesome" => "fa-hand-o-left", // Icon for the tool
+    "short_name" => "Topic Selector",
+    "description" => "This tool allows the instructor to create a number of topics that students can then select.", // Tool description
+    "messages" => array("launch", "launch_grade"),
+    "hide_from_store" => false
+);
