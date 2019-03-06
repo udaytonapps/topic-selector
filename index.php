67165786b46a0171b4d6965561e7db700ccecb9a
Dominic <donofriod1@udayton.edu>
2019-03-06 14:26:33 -0500

    Roster Tests

--- index.php
+++ index.php
@@ -132,6 +132,8 @@ $OUTPUT->flashMessages();
                                     if($topics['allow_stu'] == 1) {
                                         $count = 0;
                                         foreach($selections as $select) {
+                                            var_dump($select['user_first_name']);
+                                            var_dump($select['user_last_name']);
                                             if($count > 0) {
                                                 ?>
                                                 <span class="registeredStu">, <?=$select['user_first_name']?> <?=$select['user_last_name']?></span>
